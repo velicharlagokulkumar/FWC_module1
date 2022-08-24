680188d428ab42a0b8afc5c459504d446bf0e706
gokul kumar <104726431+velicharlagokulkumar@users.noreply.github.com>
2022-08-24 15:02:34 +0530

    Add files via upload

---
+++ Assignment-1/codes/method_1.ino
@@ -0,0 +1,33 @@
+#include <Arduino.h>
+//Declaring all variables as integers
+int X=1,Y=0,Z=0;
+int F;
+// the setup function runs once when you press reset or power the board
+void setup()
+{
+    pinMode(13, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop()
+{
+  F=(X'+Y)+(Y.Z');
+  digitalWrite(13, F);
+}
+
+#include <Arduino.h>
+//Declaring all variables as integers
+int X=1,Y=0,Z=0;
+int F;
+// the setup function runs once when you press reset or power the board
+void setup()
+{
+    pinMode(13, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop()
+{
+  F=(X'+Y)+(Y.Z');
+  digitalWrite(13, F);
+}
---
+++ Assignment-1/codes/method_2.ino
@@ -0,0 +1,16 @@
+#include <Arduino.h>
+//Declaring all variables as integers
+int X=1,Y=0,Z=0;
+int F;
+// the setup function runs once when you press reset or power the board
+void setup()
+{
+    pinMode(13, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop()
+{
+  F =(!X || Y);
+  digitalWrite(13, F);
+}
